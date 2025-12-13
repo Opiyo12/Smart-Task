e2fb2f4db8732fb1ca437b56b6de111c7e65d1c4
unknown <opiyobrian81998@gmail.com>
2025-12-13 17:35:57 +0300

    Added filtering functionality

--- src/context/TaskContext.jsx
+++ src/context/TaskContext.jsx
@@ -1,22 +1,26 @@
 import React, { createContext, useState, useEffect } from "react";
 import { getItem, setItem } from "../utils/localStorage";
+
 //create context to act as the global storage
 export const TaskContext=createContext(null);
 //create provider component
 export const TaskProvider=({children})=>{
 const[tasks,setTask]=useState(()=>
     getItem("tasks", []));
+//filtered tasks state
+const[filteredStatus, setFilteredStatus]=React.useState(tasks); 
+//deleting a specific task
+const deleteTask=(id)=>{
+  setTask((prev)=>prev.filter((task)=>task.id !==id));
+}
 //setting the persisted task to render
  useEffect(() => {
     setItem("tasks", tasks);
   }, [tasks]);
 
-const addTask=()=>{
-
-}
 return(
     <TaskContext.Provider
-    value={{tasks, addTask,setTask}}
+    value={{tasks,setTask, deleteTask, filteredStatus, setFilteredStatus}}
     >
      {children}
     </TaskContext.Provider>
